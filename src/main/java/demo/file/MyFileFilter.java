7a5c120e8d3555677272a92a5336bb99302bcee6
Peter Wang <wangxu198709@gmail.com>
2015-01-05 22:56:24 +0800

    remove some mina demo

--- src/main/java/demo/file/MyFileFilter.java
+++
@@ -1,9 +0,0 @@
-package demo.file;
-
-import org.apache.mina.filter.stream.FileRegionWriteFilter;
-
-/**
- * Created by peter on 14-12-7.
- */
-public class MyFileFilter extends FileRegionWriteFilter {
-}
